83b37245c9cb305c3ec9bd32f38d36427845b094
LIN <942801422@qq.com>
2017-04-28 17:35:40 +0800

    冒泡排序法，画图显示每一变化过程

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,18 +1,25 @@
 # -*- coding=utf-8 -*-
 # python2.7
+import matplotlib.pyplot as plt
+
+
+def BubbleSort(alist):
+    """
+    :param alist: The given list
+    :return: The sorted list
+    """
+    k = 0
+    for i in range(len(alist)):
+        k += 1
+        for j in range(len(alist)-k):
+            if alist[j] > alist[j+1]:
+                alist[j], alist[j + 1] = alist[j + 1], alist[j]
+            plt.plot(range(len(alist)), alist)
+            plt.savefig('Picture-%d%d' % (i, j))
+            plt.show()
+    return alist
 
 
 a = [5, 1, 4, 2, 8]
-def BubbleSort(alist):
-    """
-    :param alist: a given list
-    :return: a sorted list
-    """
-    for i in range(len(alist)-1):
-        for j in range(len(alist)-1):
-            if alist[j] > alist[j+1]:
-                alist[j], alist[j + 1] = alist[j + 1], alist[j]
-                print alist
-    return alist
-
-print BubbleSort(a)
+BubbleSortedList = BubbleSort(a)
+print BubbleSortedList
